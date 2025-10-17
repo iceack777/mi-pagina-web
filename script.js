93e0b67a313ddee7f16e8319016ad570d5c5f00f
iceack777 <omarpolep321@gmail.com>
2025-10-16 18:36:56 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('change-color-btn').addEventListener('click', function() {
+    document.body.style.backgroundColor = 
+        document.body.style.backgroundColor === 'lightblue' ? '#f4f4f4' : 'lightblue';
+});
